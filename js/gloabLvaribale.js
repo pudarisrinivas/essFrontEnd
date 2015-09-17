acf20fb823885b3cbe0057597e59a8a5f1ae8758
narsaiah <narsaiah.cybage@gmail.com>
2015-09-17 21:09:51 +0530

    added Java Script folder

---
+++ js/gloabLvaribale.js
@@ -0,0 +1,24 @@
+/**
+ * Created by narsaiah on 17-09-2015.
+ */
+var empNo=100;//Number
+var empName="john";//String
+var isEmp = true;//Boolean
+function getEmpSalary()
+{
+    var a=2;
+   var b= 3;
+    alert(a<<1);//true
+
+}
+function empBasic(){
+    alert(empNo);
+
+}
+function m(){
+    window.value=1000;//declaring global variable by window object
+}
+function n(){
+    alert(window.value);//accessing global variable from other function
+}
+
